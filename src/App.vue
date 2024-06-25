17a8a88969ca50159cbe23400f1d908a6726ded4
amj311 <amjudd315@gmail.com>
2024-06-25 11:29:58 -0600

    tones and first svg

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,213 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+
+  <svg width=20 height=20 stroke="black">
+    <!-- lines -->
+    <line x1="0" y1="0" x2="20" y2="0"/>
+    <line x1="0" y1="10" x2="20" y2="10" />
+    <line x1="0" y1="20" x2="20" y2="20" />
+
+    <!-- notes -->
+    <NoteBulb :note="note" x="3" y="4" />
+  </svg>
+
+&nbsp;&nbsp;
+    <select v-model="note.type">
+      <option :value="1">Whole</option>
+      <option :value="1/2">half</option>
+      <option :value="1/4">quarter</option>
+      <option :value="1/8">eighth</option>
+      <option :value="1/16">sixteenth</option>
+    </select>
+    
+    
+   
+    <br /><br />
+    
+  
+    <select v-model="keyOffset">
+      <option v-for="note in 12" :value="note - 1">{{getNote(note - 1).name}}</option>
+    </select>
+    <table>
+      <tr v-for="step in scale" @click="playTone(getFrequency(step.offset + keyOffset))" :style="{ opacity: step.isHalf ? .5 : ''}">
+        <td>{{step.name}}</td>
+        <td>{{getNote(step.offset + keyOffset).name}}</td>
+      </tr>
+    </table>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup>
+import { ref, reactive } from 'vue'; 
+import NoteBulb from './components/NoteBulb.vue';
+
+  const note = reactive({
+    type: 1,
+    // dotted: false,
+  });
+  
+const keyOffset = ref(0);
+const middleCHz = 261.63;
+const getFrequency = noteNumber => middleCHz * Math.pow(2, noteNumber / 12);
+
+const scale = [
+  {
+    name: 'do',
+    offset: 0,
+  },
+  {
+    name: 'di',
+    isHalf: true,
+    offset: 1,
+  },
+  {
+    name: 're',
+    offset: 2,
+  },
+  {
+    name: 'ri',
+    isHalf: true,
+    offset: 3,
+  },
+  {
+    name: 'mi',
+    offset: 4,
+  },
+  {
+    name: 'fa',
+    offset: 5,
+  },
+  {
+    name: 'fi',
+    isHalf: true,
+    offset: 6,
+  },
+  {
+    name: 'sol',
+    offset: 7,
+  },
+  {
+    name: 'si',
+    isHalf: true,
+    offset: 8,
+  },
+  {
+    name: 'la',
+    offset: 9,
+  },
+  {
+    name: 'li',
+    isHalf: true,
+    offset: 10,
+  },
+  {
+    name: 'ti',
+    offset: 11,
+  },
+  {
+    name: 'do',
+    offset: 12,
+  },
+]
+
+const notes = [
+  {
+    name: 'C',
+  },
+  {
+    isHalf: true,
+    name: 'C#',
+  },
+  {
+    name: 'D',
+  },
+  {
+    isHalf: true,
+    name: 'D#',
+  },
+  {
+    name: 'E',
+  },
+  {
+    name: 'F',
+  },
+  {
+    isHalf: true,
+    name: 'F#',
+  },
+  {
+    name: 'G',
+  },
+  {
+    isHalf: true,
+    name: 'G#',
+  },
+  {
+    name: 'A',
+  },
+  {
+    isHalf: true,
+    name: 'A#',
+  },
+  {
+    name: 'B',
+  },
+]
+
+// resolves any +/- integer to a note relative middle C at 0
+const getNote = noteNumber => notes[(12 + (noteNumber % 12)) % 12];
+  
+// create web audio api context
+var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+
+function playTone(frequency, duration) {
+  // create Oscillator node
+  var oscillator = audioCtx.createOscillator();
+
+  try {
+  oscillator.frequency.value = frequency; // value in hertz
+  oscillator.connect(audioCtx.destination);
+  oscillator.start();
+  
+    alert(frequency);  
+    
+  }
+  catch (e) {
+    console.log(e)
+    alert(e)
+    }
+  
+  return new Promise((res) => {
+    setTimeout(
+    function() {
+      oscillator.stop();
+      res();
+    }, duration);
+  });
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  
+  
+</script>
+
+<!-- Use preprocessors via the lang attribute! e.g. <style lang="scss"> -->
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+a,
+button {
+  color: #4fc08d;
+}
+
+button {
+  background: none;
+  border: solid 1px;
+  border-radius: 2em;
+  font: inherit;
+  padding: 0.75em 2em;
 }
 </style>
---
+++ src/components/NoteBulb.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  note: Object,
+  x: Number,
+  y: Number,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <!--  single note no stem -->
+  <svg :x="x" :y="y" width=14 height=12 stroke="black">
+    <g style="transform-origin: center;" :style="{ transform: note.type < 1 ? 'rotate(-30deg) scale(0.9)' : ''}">
+      <path stroke="black" stroke-width="0"
+        d="M 6.5,0 a 7,6 0 1,0 1,0 z     M 6.5,3 a 6,3 0 1,0 1,0 z" :fill-rule="note.type < 1/2 ? null : 'evenodd'" fill="black" />
+    </g>
+<!--         <line x1=1 y1=9 x2=1 y2=22 stroke-width=1.5 /> -->
+  </svg>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
